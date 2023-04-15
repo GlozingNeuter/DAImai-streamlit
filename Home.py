34d8ac56512f1b009f57d0a858ba3f200b502a75
Olivier Gason <98195215+GlozingNeuter@users.noreply.github.com>
2023-04-15 16:26:10 +0200

    Add exemple on landing page

--- Home.py
+++ Home.py
@@ -5,5 +5,19 @@ from streamlit_folium import st_folium, folium_static
 import pandas as pd
 import folium
 from folium.plugins import GroupedLayerControl, BeautifyIcon, AntPath
+import streamlit as st
 
-st.markdown("# DAI DAI DAI")
+st.set_page_config(
+    page_title="Hello",
+    )
+
+st.markdown(
+    """
+    # DAImap
+
+    ### Tool to map archive data
+
+    """
+)
+st.components.v1.iframe("https://kipple.be/d3-fig/map.html", height=700)
+
